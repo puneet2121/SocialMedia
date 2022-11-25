b663ac41ab33f81335b56605420c997cee732a13
puneet2121 <bedipuneet21@gmail.com>
2022-11-24 22:46:52 -0700

    set up the starting files

---
+++ Back-end/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import cors from "cors";
+import helmet from 'helmet';
+import dotenv from 'dotenv';
