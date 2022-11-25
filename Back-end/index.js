3c8ec8489b1ac8773c7e47df9db017eb8b72a7a0
puneet2121 <bedipuneet21@gmail.com>
2022-11-25 00:45:01 -0700

    add database

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,7 +1,46 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import morgan from "morgan";
 import cors from "cors";
-import helmet from 'helmet';
-import dotenv from 'dotenv';
+import helmet from "helmet";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import multer from "multer";
+
+//config
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json);
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({ policy: "cross-origin" }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// File storage
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+//database mongoDB
+const PORT = process.env.PORT || 6000
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((err) => console.log(`${err} did not save the data`));
